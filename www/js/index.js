ad7e74e2d7a5768e2a1ab83ba04f9d408a22d8df
Navdeep Singh <info@cyfersystems.com>
2015-03-16 08:33:58 -0700

    Web - App banner to prompt install on iOS and Android Web - Pinnable site for Chrome, iOS, Windows, where supported Increased clickable area for zoom buttons New engine for detecting gestures Google Analytics Hide settings on click anywhere outside settings when open

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,10 @@ function onDeviceReady() {
   } else {
     StatusBar.styleDefault();
   }
+  window.localStorage["no_smartbanner"] = 1;
+  $("#smartbanner").remove();
+  $("body").removeClass("smartbanner");
+  $("script[src='js/jquery.smartbanner.js']").remove();
 }
 
 var swipe_nav = window.localStorage["swipe_nav"] || 1;
@@ -65,44 +69,68 @@ $(function() {
 
   $("#settings_button").click(function() {
     $("#settings").toggle();
-  })
-  $("body").on("click", ".bigger", function () {
+    ga('send','event','button','click','settings'); 
+  });
+  $("*").on("click", function(e){
+    if ($(e.target).attr("id") !== "settings_button" && $(e.target).parents("#settings_button").length < 1 && $("#settings").is(":visible")) {
+      if($(e.target).parents("#settings").length < 1) {
+        $("#settings").toggle();
+      }
+    }
+  });
+  $(".bigger").click(function () {
     font_size += 1;
     $("#paatth").css("font-size", font_size + "px");
     window.localStorage["font_size"] = font_size;
+    ga('send','event','setting','change','zoom','in');
   });
-  $("body").on("click", ".smaller", function () {
+  $(".smaller").click(function () {
     font_size -= 1;
     $("#paatth").css("font-size", font_size + "px");
     window.localStorage["font_size"] = font_size;
+    ga('send','event','setting','change','zoom','out');
   });
-  $("body").on("submit", ".submit_ang", function(event) {
-    setAng($(this).find("input").val());
+  $(".submit_ang").submit(function(event) {
+    ang = $(this).find("input").val();
+    setAng(ang);
+    ga('send','event','submit','ang',ang);
     $('.ang').blur();
     event.preventDefault();
   });
-  $("body").on("click", "#navigation a.minus1, #navigation a.plus1", function () {
+  $("#navigation a.minus1, #navigation a.plus1").click(function () {
     setAng($(this).data("ang"));
+    if ($(this).hasClass('minus1')) {
+      ga('send','event','button','click','nav arrows','prev');
+    } else {
+      ga('send','event','button','click','nav arrows','next');
+    }
   });
-  $("body").on("focus", ".ang", function(){
+  $(".ang").focus(function(){
     $(this).select();
   })
   $(".setting").click(function () {
     setting = $(this).data("setting");
-    if ($(this).data("on") == "0") {
+    data_on = $(this).data("on");
+    if (data_on == "0") {
       window.localStorage[setting] = 1;
       $(this).data("on", "1");
       switch(setting) {
         case "larreevaar_assistance":
           $(this).addClass(setting);
+          $("#paatth").addClass(setting);
+          ga('send','event','button','click','assist','on');
+          break;
         case "larreevaar":
           $("#paatth").addClass(setting);
+          ga('send','event','setting','change','larreevaar','on');
           break;
         case "swipe_nav":
           swipe_nav = 1;
+          ga('send','event','setting','change','swipe','on');
           break;
         case "dark":
           $("body").addClass(setting);
+          ga('send','event','setting','change','dark','on');
           break;
       }
       if ($(this).hasClass("checkbox")) {
@@ -114,14 +142,20 @@ $(function() {
       switch(setting) {
         case "larreevaar_assistance":
           $(this).removeClass(setting);
+          $("#paatth").removeClass(setting);
+          ga('send','event','button','click','assist','off');
+          break;
         case "larreevaar":
           $("#paatth").removeClass(setting);
+          ga('send','event','setting','change','larreevaar','off');
           break;
         case "swipe_nav":
           swipe_nav = 0;
+          ga('send','event','setting','change','swipe','off');
           break;
         case "dark":
           $("body").removeClass(setting);
+          ga('send','event','setting','change','dark','off');
           break;
       }
       if ($(this).hasClass("checkbox")) {
@@ -129,139 +163,30 @@ $(function() {
       }
     }
   });
+  if (!window.localStorage['no_smartbanner']) {
+    $.smartbanner({
+      layer: true,
+      speedIn: 400
+    });
+  }
+  if (swipe_nav) {
+    var myElement = document.getElementById('paatth');
+    var mc = new Hammer(myElement);
+    mc.get("swipe").set({
+      direction: Hammer.DIRECTION_HORIZONTAL,
+      velocity: 0.2
+    })
+    mc.on("swipeleft swiperight", function(ev) {
+      switch(ev.type) {
+        case 'swiperight':
+          $(".minus1").click();
+          ga('send','event','swipe','right');
+          break;
+        case "swipeleft":
+          $(".plus1").click();
+          ga('send','event','swipe','left');
+          break;
+      }
+    });
+  }
 });
-// TOUCH-EVENTS SINGLE-FINGER SWIPE-SENSING JAVASCRIPT
-// Courtesy of PADILICIOUS.COM and MACOSXAUTOMATION.COM
-
-// this script can be used with one or more page elements to perform actions based on them being swiped with a single finger
-
-var triggerElementID = null; // this variable is used to identity the triggering element
-var fingerCount = 0;
-var startX = 0;
-var startY = 0;
-var curX = 0;
-var curY = 0;
-var deltaX = 0;
-var deltaY = 0;
-var horzDiff = 0;
-var vertDiff = 0;
-var minLength = 72; // the shortest distance the user may swipe
-var swipeLength = 0;
-var swipeAngle = null;
-var swipeDirection = null;
-
-// The 4 Touch Event Handlers
-
-// NOTE: the touchStart handler should also receive the ID of the triggering element
-// make sure its ID is passed in the event call placed in the element declaration, like:
-// <div id="picture-frame" ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
-
-function touchStart(event,passedName) {
-  if (swipe_nav) {
-    // disable the standard ability to select the touched object
-    //event.preventDefault();
-    // get the total number of fingers touching the screen
-    fingerCount = event.touches.length;
-    // since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
-    // check that only one finger was used
-    if ( fingerCount == 1 ) {
-      // get the coordinates of the touch
-      startX = event.touches[0].pageX;
-      startY = event.touches[0].pageY;
-      // store the triggering element ID
-      triggerElementID = passedName;
-    } else {
-      // more than one finger touched so cancel
-      touchCancel(event);
-    }
-  }
-}
-
-function touchMove(event) {
-  if (swipe_nav) {
-    //event.preventDefault();
-    if ( event.touches.length == 1 ) {
-      curX = event.touches[0].pageX;
-      curY = event.touches[0].pageY;
-    } else {
-      touchCancel(event);
-    }
-  }
-}
-
-function touchEnd(event) {
-  if (swipe_nav) {
-    //event.preventDefault();
-    // check to see if more than one finger was used and that there is an ending coordinate
-    if ( fingerCount == 1 && curX != 0 ) {
-      // use the Distance Formula to determine the length of the swipe
-      swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
-      // if the user swiped more than the minimum length, perform the appropriate action
-      if ( swipeLength >= minLength ) {
-        caluculateAngle();
-        determineSwipeDirection();
-        processingRoutine();
-        touchCancel(event); // reset the variables
-      } else {
-        touchCancel(event);
-      } 
-    } else {
-      touchCancel(event);
-    }
-  }
-}
-
-function touchCancel(event) {
-  // reset the variables back to default values
-  fingerCount = 0;
-  startX = 0;
-  startY = 0;
-  curX = 0;
-  curY = 0;
-  deltaX = 0;
-  deltaY = 0;
-  horzDiff = 0;
-  vertDiff = 0;
-  swipeLength = 0;
-  swipeAngle = null;
-  swipeDirection = null;
-  triggerElementID = null;
-}
-
-function caluculateAngle() {
-  var X = startX-curX;
-  var Y = curY-startY;
-  var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
-  var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
-  swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
-  if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
-}
-
-function determineSwipeDirection() {
-  if ( (swipeAngle <= 25) && (swipeAngle >= 0) ) {
-    swipeDirection = 'left';
-  } else if ( (swipeAngle <= 360) && (swipeAngle >= 335) ) {
-    swipeDirection = 'left';
-  } else if ( (swipeAngle >= 155) && (swipeAngle <= 205) ) {
-    swipeDirection = 'right';
-  } else if ( (swipeAngle > 25) && (swipeAngle < 155) ) {
-    swipeDirection = 'down';
-  } else {
-    swipeDirection = 'up';
-  }
-}
-
-function processingRoutine() {
-  var swipedElement = document.getElementById(triggerElementID);
-  if ( swipeDirection == 'left' ) {
-    $(".plus1").click();
-  } else if ( swipeDirection == 'right' ) {
-    $(".minus1").click();
-  } else if ( swipeDirection == 'up' ) {
-    // REPLACE WITH YOUR ROUTINES
-    //swipedElement.style.backgroundColor = 'maroon';
-  } else if ( swipeDirection == 'down' ) {
-    // REPLACE WITH YOUR ROUTINES
-    //swipedElement.style.backgroundColor = 'purple';
-  }
-}
