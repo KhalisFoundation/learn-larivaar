22adb267fec9bc1af2da9d23b186a7dbc433e10a
Navdeep Singh <me@navdeepsingh.ca>
2019-03-11 07:15:22 -0700

    Combine Paatth and PaatthContainer

--- src/components/Paatth/Paatth.js
+++ src/components/Paatth/Paatth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -9,55 +9,40 @@ import { fetchAngAction } from '../../actions/ang';
 import styles from './Paatth.css';
 
 const Paatth = ({
-  verses,
-}) => (
-  <div id="paatth" className={styles.paatth}>
-    {verses && verses.map(verse => (
-      verse.verse.split(' ').map((val, index) => {
-        if (val.indexOf('॥') !== -1) {
-          return (
-            <React.Fragment key={`sbd-${verse.ID}-${index}`}>
-              <i>{val}</i>
-              {' '}
-            </React.Fragment>
-          );
-        }
-        return (
-          <React.Fragment key={`sbd-${verse.ID}-${index}`}>
-            <span>{val}</span>
-            {' '}
-          </React.Fragment>
-        );
-      })
-    ))}
-  </div>
-);
-Paatth.propTypes = {
-  verses: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.shape({})),
-    PropTypes.bool,
-  ]),
-};
-Paatth.defaultProps = {
-  verses: false,
-};
-
-class PaatthContainer extends React.Component {
-  componentDidMount() {
-    const { angCache, currentAng: { ang, source }, fetchAng } = this.props;
+  angCache,
+  currentAng: { ang, source },
+  fetchAng,
+}) => {
+  useEffect(() => {
     if (!angCache[source][ang]) {
       fetchAng(ang, source);
     }
-  }
-
-  render() {
-    const { angCache, currentAng: { ang, source } } = this.props;
-    return (
-      <Paatth verses={angCache[source][ang]} />
-    );
-  }
-}
-PaatthContainer.propTypes = {
+  });
+  const verses = angCache[source][ang] || false;
+  return (
+    <div id="paatth" className={styles.paatth}>
+      {verses && verses.map(verse => (
+        verse.verse.split(' ').map((val, index) => {
+          if (val.indexOf('॥') !== -1) {
+            return (
+              <React.Fragment key={`sbd-${verse.ID}-${index}`}>
+                <i>{val}</i>
+                {' '}
+              </React.Fragment>
+            );
+          }
+          return (
+            <React.Fragment key={`sbd-${verse.ID}-${index}`}>
+              <span>{val}</span>
+              {' '}
+            </React.Fragment>
+          );
+        })
+      ))}
+    </div>
+  );
+};
+Paatth.propTypes = {
   angCache: PropTypes.shape({}).isRequired,
   currentAng: PropTypes.shape({}).isRequired,
   fetchAng: PropTypes.func.isRequired,
@@ -72,4 +57,4 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   fetchAng: fetchAngAction,
 }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(PaatthContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(Paatth);
