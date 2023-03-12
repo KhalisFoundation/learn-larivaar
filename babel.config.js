c61e5963bd7631c6866b60a8c0ca8c40bfabb4b4
Gurmehar Singh <gurmeharsinghkhalsa1005@gmail.com>
2023-03-12 11:52:21 +1100

    fix: updated packages and changed env to expo react native
    updated node packages and changed env to expo react native for easy testing/building.
    the app works only on iOS rn

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin'
+    ],
+  };
 };
