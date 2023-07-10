2b6c89503bb82a2e865343a6aec4f7d4aca56a3d
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2023-07-10 22:45:44 +0530

    feat: add a loading text on changing ang

--- components/Ang/index.tsx
+++ components/Ang/index.tsx
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import {AngProps, AngData} from './interfaces';
 import {layoutStyles} from '../../styles/layout';
 import {bakePankti} from './utils/bakePankti';
@@ -8,12 +8,21 @@ import {LarivaarContext} from '../../context';
 const Ang = (props: AngProps): JSX.Element => {
   const [currentAngData, setCurrentAngData] = useState({} as AngData);
   const {larivaarAssist, larivaar} = useContext(LarivaarContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`https://api.banidb.com/v2/angs/${props.page}`)
       .then(res => res.json())
-      .then(data => setCurrentAngData(data));
+      .then(data => {
+        setIsLoading(false);
+        setCurrentAngData(data);
+      });
   }, [props.page]);
+
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
 
   return (
     <View style={layoutStyles.wordContainer}>
